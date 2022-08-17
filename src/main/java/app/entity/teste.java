f2b3687cc286e76cb249e54f87fa44f59523b58b
Guilherme Mateus Do Carmo <guilherme.carmo@cronapp.io>
2022-08-17 12:30:22 +0000

    First commit...

---
+++ src/main/java/app/entity/teste.java
@@ -0,0 +1,138 @@
+
+package app.entity;
+
+import java.io.*;
+import javax.persistence.*;
+import java.util.*;
+import javax.xml.bind.annotation.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonFilter;
+import cronapi.rest.security.CronappSecurity;
+import cronapi.swagger.CronappSwagger;
+
+
+/**
+* Classe que representa a tabela TESTE
+* @generated
+*/
+@javax.persistence.Entity
+@javax.persistence.Table(name = "\"TESTE\"")
+@XmlRootElement
+@CronappSecurity
+@JsonFilter("app.entity.teste")
+public class teste implements Serializable {
+    /**
+    * UID da classe, necessário na serialização
+    * @generated
+    */
+    private static final long serialVersionUID = 1L;
+
+    /**
+    * @generated
+    */
+    @Id
+    @Column(name = "id", nullable = false, insertable=true, updatable=true)
+        private java.lang.String id = UUID.randomUUID().toString().toUpperCase();
+
+
+    /**
+    * @generated
+    */
+    @Column(name = "name", nullable = true, unique = false, insertable=true, updatable=true)
+        
+        private java.lang.String name;
+
+
+    /**
+    * @generated
+    */
+    @Column(name = "attribute02", nullable = true, unique = false, insertable=true, updatable=true)
+        
+        private java.lang.String attribute02;
+
+
+    /**
+    * Construtor
+    * @generated
+    */
+    public teste(){
+    }
+
+    /**
+    * Obtém id
+    * return id
+    * @generated
+    */
+    public java.lang.String getId() {
+        return this.id;
+    }
+
+    /**
+    * Define id
+    * @param id id
+    * @generated
+    */
+    public teste setId(java.lang.String id) {
+        this.id = id;
+        return this;
+    }
+    /**
+    * Obtém name
+    * return name
+    * @generated
+    */
+    public java.lang.String getName() {
+        return this.name;
+    }
+
+    /**
+    * Define name
+    * @param name name
+    * @generated
+    */
+    public teste setName(java.lang.String name) {
+        this.name = name;
+        return this;
+    }
+    /**
+    * Obtém attribute02
+    * return attribute02
+    * @generated
+    */
+    public java.lang.String getAttribute02() {
+        return this.attribute02;
+    }
+
+    /**
+    * Define attribute02
+    * @param attribute02 attribute02
+    * @generated
+    */
+    public teste setAttribute02(java.lang.String attribute02) {
+        this.attribute02 = attribute02;
+        return this;
+    }
+
+    /**
+    * @generated
+    */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+teste object = (teste)obj;
+        if (id != null ? !id.equals(object.id) : object.id != null) return false;
+        return true;
+    }
+
+    /**
+    * @generated
+    */
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = 31 * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+}
---
+++ src/main/webapp/js/blockly/teste.blockly.js
@@ -0,0 +1,19 @@
+window.blockly = window.blockly || {};
+window.blockly.js = window.blockly.js || {};
+window.blockly.js.blockly = window.blockly.js.blockly || {};
+window.blockly.js.blockly.teste = window.blockly.js.blockly.teste || {};
+
+/**
+ * @function Executar
+ *
+ * teste
+ *
+ *
+ * @author Guilherme Mateus Do Carmo
+ * @since 16/08/2022 11:42:47
+ *
+ */
+window.blockly.js.blockly.teste.ExecutarArgs = [];
+window.blockly.js.blockly.teste.Executar = async function() {
+
+}
